aeff3ba8f412840643411327bf95037c42d08bd0
S.M JEE <jisungmin990@naver.com>
2025-11-25 06:17:43 +0000

    add source

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+extern int add(int a, int b);
+extern int sub(int a, int b);
+
+
+int main() {
+int num1, num2, res;
+
+unm1 = 10;
+num2 = 20;
+
+res = add(num1, num2);
+printf("%d + %d = %d\n", num1, num2, res);
+res = sub(num1, num2);
+printf("%d - %d = %d\n", num1, num2, res);
+}
+
+
---
+++ sub.c
@@ -0,0 +1,14 @@
+#incldue <stdio.h>
+
+int sub(int n1, int n2)
+
+{
+
+  int num;
+  
+  num = n1 - n2;
+ return num;
+
+}
+
+
